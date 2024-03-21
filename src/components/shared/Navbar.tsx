a71fdc9e02a85d15b39297642c8094c296443eb1
Bhanu <bhanusngh12345@gmail.com>
2024-03-21 19:24:31 +0530

    ui layout done

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-16 fixed bg-black text-white'>
+        <p>teri</p>
+    </div>
+  )
+}
+
+export default Navbar
